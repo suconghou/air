8d5b341bcbddce60c0328a1534347cc7dd858d3f
suconghou <1126045770@qq.com>
2016-12-21 20:23:07 +0800

    update timeout

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ var app=
 		instance.get('/reload',function(req,res,next)
 		{
 			var url='//'+req.headers.host+'/reload';
-			var reload='var a=new XMLHttpRequest;a.open("POST","'+url+'",1),a.onreadystatechange=function(){4==a.readyState&&location.reload()},a.send();';
+			var reload='var a=new XMLHttpRequest;a.timeout=3e6;a.open("POST","'+url+'",1),a.onreadystatechange=function(){4==a.readyState&&location.reload()},a.send();';
 			res.type('js').send(reload);
 		});
 		instance.post('/reload',function(req,res,next)
@@ -945,7 +945,7 @@ var service=
 	{
 		port:8088,
 		debug:true,
-		version:'0.4.15',
+		version:'0.4.16',
 		cfgname:'static.json',
 		workPath:process.cwd(),
 		nodePath:process.env.NODE_PATH,
