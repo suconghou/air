497ae1482be881f330e89d7880e77731d15ada99
suconghou <1126045770@qq.com>
2019-05-14 10:41:35 +0800

    fix: lint help

--- src/config.js
+++ src/config.js
@@ -1,25 +1,27 @@
 export default `
 Usage:
-	air [command] [flag]
+    air [command] [flag]
 Commands:
-	serve		  	start air http server
-	lint			eslint js
-	compress		compress less or javascript files
-	install			install git hooks
-	template 		use art-template render html
+    serve           start air http server
+    lint            eslint js
+    compress        compress less or javascript files
+    install         install git hooks
+    template        use art-template render html
     
 Flags:
-	-v     			show air version
-	-h      		show this help information
-	-p     			set server listen port
-	-d     			set server document root
-	-dir  			set lint or install config path
-	--debug			compress with debug mode
-	--clean			compress with clean mode,remove console debugger
-	--escape		escape when use template
-	--dry  			just run as a static server
-	--art  			use art-template not ssi
-	--lint  		lint only,useful for air lint
+    -v              show air version
+    -h              show this help information
+    -p              set server listen port
+    -d              set server document root
+    -dir            set lint or install config path
+    --debug         compress with debug mode
+    --clean         compress with clean mode,remove console debugger
+    --escape        escape when use template
+    --dry           just run as a static server
+    --art           use art-template not ssi
+    --lint          lint only,useful for air lint
+    --noprettier    for air lint & air gitlint , do not run prettier task
+    --noeslint      for air lint & air gitlint , do not run eslint task
 `;
 
 export const version = '0.6.30';
